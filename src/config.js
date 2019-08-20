f2439e6c3828d8b32b8082ed1c9129ca177385d1
Shaun Wassell <shaun@trove.ai>
2019-08-19 21:55:54 -0400

    Adding Stripe keys to config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 const dev = {
+  STRIPE_KEY: "pk_test_TnLuhUx3s2kVwvVVh9xwhhto00s17I4ur6",
   s3: {
     REGION: "us-east-1",
     BUCKET: "notes-app-2-api-dev-attachmentsbucket-9ovej89orv68"
@@ -16,6 +17,7 @@ const dev = {
 };
 
 const prod = {
+  STRIPE_KEY: "pk_test_TnLuhUx3s2kVwvVVh9xwhhto00s17I4ur6",
   s3: {
     REGION: "us-east-1",
     BUCKET: "notes-app-2-api-prod-attachmentsbucket-32i52ajqqi8v"
